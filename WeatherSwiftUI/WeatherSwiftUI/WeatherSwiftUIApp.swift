a9b5841d6060d0cb37c10cbbd890dbc637b2e23d
Raj Anand <raj.anand0908@gmail.com>
2023-01-07 20:31:13 +0530

    [TASK] Added Project

---
+++ WeatherSwiftUI/WeatherSwiftUI/WeatherSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSwiftUIApp.swift
+//  WeatherSwiftUI
+//
+//  Created by Raj Anand on 07/01/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
