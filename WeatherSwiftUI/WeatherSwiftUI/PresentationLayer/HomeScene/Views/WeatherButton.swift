c931af703d08904332abff8ae7df7b49eb1fa1a0
Raj Anand <raj.anand0908@gmail.com>
2023-01-08 21:05:35 +0530

    [TASK] Added weather change button

---
+++ WeatherSwiftUI/WeatherSwiftUI/PresentationLayer/HomeScene/Views/WeatherButton.swift
@@ -0,0 +1,25 @@
+//
+//  WeatherButton.swift
+//  WeatherSwiftUI
+//
+//  Created by Raj Anand on 08/01/23.
+//
+
+import SwiftUI
+
+struct WeatherButton: View {
+  
+  var title: String
+  var textColor: Color
+  var backgroundColor: Color
+  
+  var body: some View {
+    Text(title)
+      .frame(width: 280, height: 50)
+      .background(backgroundColor)
+      .foregroundColor(textColor)
+      .font(.system(size: 20, weight: .bold, design: .default))
+      .cornerRadius(10)
+  }
+  
+}
